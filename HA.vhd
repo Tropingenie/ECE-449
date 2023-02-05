517aecfa4075aa6c3f6db906b637b975509631f6
Pon Pon <steeevie22@gmail.com>
2023-02-04 16:12:44 -0800

    Write (untested) adder

---
+++ HA.vhd
@@ -0,0 +1,29 @@
+-----------------------------------------------------------
+--
+-- Half Adder
+--
+-- 1-bit half adder component
+--
+-- B. Lyne
+--
+-----------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity HA is 
+port (
+		x1, x2        : in STD_LOGIC;
+		y, cout : out STD_LOGIC
+	);
+end HA;
+
+architecture structure of HA is
+
+begin       -- note that all of the following statements are concurrent
+       
+
+    y <=  x1 xor x2; -- sum
+    cout <=  ; x1 and x2 -- carry
+    
+ end structure;
