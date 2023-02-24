db247ad0b3efb6644f0ec90d252a988d265fd5f4
Kai Herrero <Kai.herrero@gmail.com>
2023-02-23 21:17:07 -0800

    Updated ALU.vhd to include entire opcode and created register_arbitrator file

--- ALU/ALU.vhd
+++ ALU/ALU.vhd
@@ -18,10 +18,9 @@ entity ALU is
 
 port ( 
     in1, in2 : in STD_LOGIC_VECTOR(15 downto 0);  --Input 1, Input 2 16-bit inputs including op code and fortmat A(0-3)
-    alu_mode : in STD_LOGIC_VECTOR(2 downto 0);   --3 bit vector that determines the ALU operation [ADD (0), ADD(1), SUB (2), MUL(3), NAND(4), SHL (5), SHR(6), TEST(7)]
+    op_code  : in STD_LOGIC_VECTOR(6 downto 0);   --Op code of current instuction, used to deduce operation in ALU
     clk, rst : in STD_LOGIC;                      --clock and reset signals
     result   : out STD_LOGIC_VECTOR(15 downto 0); --3 bit result vector
-    --check : out signed(15 downto 0);
     z_flag, n_flag, o_flag: out STD_LOGIC         --zero, negative and overflow flags
     );
 end ALU;
@@ -29,18 +28,18 @@ end ALU;
 architecture Behavioral of ALU is
 
     --SIGNAL DECLARATIONS HERE--
-    signal data1     : signed(15 downto 0); --in1 signed 16 bit vector
-    signal data2     : signed(15 downto 0); --in2 signed 16 bit vector
-    signal data3     : signed(15 downto 0); --result 16 bit signed vector
-    signal mult_ofr  : signed (31 downto 0);--cheat 32 bit internal register for multiply overflow
-    signal shift_amt : integer;             --integer amount to shift
+    signal data1     : signed(15 downto 0);      --in1 signed 16 bit vector
+    signal data2     : signed(15 downto 0);      --in2 signed 16 bit vector
+    signal data3     : signed(15 downto 0);      --result 16 bit signed vector
+    signal mult_ofr  : signed (31 downto 0);     --cheat 32 bit internal register for multiply overflow
+    signal shift_amt : integer;                  --integer amount to shift
         
     --COMPONENT DECLARATIONS HERE--
     
 begin
 
-    data1 <= signed(in1(15 downto 0));               --input in1 vector onto data1 as a signed bit
-    data2 <= signed(in2(15 downto 0));               --input in2 vector onto data2 as a signed bit
+    data1 <= signed(in1(15 downto 0));           --input in1 vector onto data1 as a signed bit
+    data2 <= signed(in2(15 downto 0));           --input in2 vector onto data2 as a signed bit
     
     process(clk,rst) 
     
@@ -52,12 +51,12 @@ begin
     o_flag <= '0';
     
     elsif(RISING_EDGE(clk)) then
-        case alu_mode is 
+        case op_code is 
         
-            when "000" =>
-                data3 <= (others => '0');                   --NOP
+            when "0000000" =>                    --NOP
+                data3 <= (others => '0');        
                 
-            when "001" =>                                   --ADD op
+            when "0000001" =>                    --ADD op
                 if (data2 + data1) = 0 then
                     data3 <= data1 + data2;
                     o_flag <= '1';
@@ -65,7 +64,7 @@ begin
                     data3 <= data1 + data2;                     
                 end if;
                 
-            when "010" =>                                   --SUB op
+            when "0000010" =>                    --SUB op
                 if( to_integer(signed(data2)) > to_integer(signed(data1)) ) then
                     n_flag <= '1';
                 elsif( to_integer(signed(data1)) - to_integer(signed(data2)) = 0 ) then
@@ -74,7 +73,7 @@ begin
                     data3 <= data1 - data2;
                 end if;                   
                 
-            when "011" =>                                   --MULT op
+            when "0000011" =>                    --MULT op
                 if( (to_integer(signed(data1))) * (to_integer(signed(data2))) > integer(65536)) then
                     o_flag <= '1';
                 elsif( to_integer(signed(data1)) * to_integer(signed(data2)) = 0) then
@@ -88,31 +87,36 @@ begin
                     data3 <= mult_ofr(15 downto 0);                     
                 end if;
               
-            when "100" => 
-                data3 <= data1 NAND data2;                  --NAND op
+            when "0000100" =>                    --NAND op
+                data3 <= data1 NAND data2;       
                 
-            when "101" =>                                   --SHFT R op
-                shift_amt <= to_integer(unsigned(in1(8 downto 6)));
-                data3 <= SHIFT_LEFT(data1, shift_amt);
+            when "00000101" =>                   --SHFT R op
+                shift_amt <= to_integer(unsigned(in2(3 downto 0)));
+                data3 <= SHIFT_LEFT(data1, shift_amt);      --currently sign extends for 2's complement
                 
-            when "110" =>                                   --SHFT L op
-                shift_amt <= to_integer(unsigned(in1(8 downto 6)));
+            when "0000110" =>                    --SHFT L op
+                shift_amt <= to_integer(unsigned(in2(3 downto 0)));
                 data3 <= SHIFT_RIGHT(data1, shift_amt);     --currently sign extends
                 
-            when "111" =>                                   --TEST Op
+            when "0000111" =>                    --TEST Op
                 if (data1 = b"0000000000000000") then
                     z_flag <= '1';
-                elsif data1(15) = '1' then
+                elsif data1(15) = '1' and data(14) = '1' then
                     n_flag <= '1';
                 end if;
+
+            when "0100000" =>                    --OUT Op
+            --??
+
+            when "0100001" =>                    --IN Op
+            --???
                     
             when others =>
                 assert false report "ALU operation out of range" severity failure;
                 
            end case;
            
-        result <= STD_LOGIC_VECTOR(data3(15 downto 0)); --Typecast doesnt work
-        --check <= data3;                                 --REMOVE THIS
+        result <= STD_LOGIC_VECTOR(data3(15 downto 0));     --Typecast data3 back into result vector
         
         end if;         
     end process; 
--- Controller/register_arbitrator.vhd
+++ Controller/register_arbitrator.vhd
@@ -7,12 +7,13 @@
 -- sends a "pause" signal to stop the pipeline for two
 -- register instructions.
 --
--- Kai Herrero
+-- Engineer: Kai Herrero, Benjamin Lyne
 --
 -----------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity RegisterArbitrator is
 port(
@@ -21,5 +22,36 @@ port(
     AR          : in std_logic_vector(15 downto 0); -- Arithmetic result (data) from the "wiring back" instruction
     ra          : in std_logic_vector(2 downto 0); -- writeback register of the "writing back" instruction
     rd_2        : in std_logic_vector(2 downto 0); -- Second register from "current" instruction
+    clk         : in std_logic;                     -- Clock at twice the rate of the datapath
+    wr_en       : out std_logic;                    -- Write enable for writeback
+    r_sel       : out std_logic_vector(2 downto 0); -- Selected register
+
 );
 end RegisterArbitrator;
+
+architecture Behavioral of RegisterArbitrator is
+
+--SIGNAL DECLARATIONS HERE--
+    signal register_a  : STD_LOGIC_VECTOR(6 downto 0);
+    signal register_d2 : STD_LOGIC_VECTOR(2 downto 0);
+    signal op_in       : STD_LOGIC_VECTOR(6 downto 0);
+    signal op_back     : STD_LOGIC_VECTOR(6 downto 0);
+    signal result      : STD_LOGIC_VECTOR(15 downto 0);
+    signal write_enable: STD_LOGIC
+--COMPONENT DECLARATIONS HERE--
+
+begin
+
+    register_a  <= ra;
+    register_d2 <= rd_2;
+    op_in       <= opcode_in;
+    op_back     <= opcode_back;
+    result      <= AR;
+    wr_en       <= 
+
+    process(clk)
+
+    begin
+       
+    end process;
+end Behavioral;
