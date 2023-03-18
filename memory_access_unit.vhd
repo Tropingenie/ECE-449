fa26277bed0c5a6b14a40b657b970a626a1b2638
Ben Lyne <blyne5658@uvic.ca>
2023-03-18 11:48:43 -0700

    Implement memory data flow and control modules

--- memory_access_unit.vhd
+++ memory_access_unit.vhd
@@ -3,53 +3,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity MemoryAccessUnit is
 port(
-    opcode  : in        std_logic_vector(6 downto 0);
-    AR      : in        std_logic_vector(15 downto 0);  -- Result from ALU
-    M_ADDR  : out       std_logic_vector(15 downto 0);  -- Address line to memory
-    M_DATA  : inout     std_logic_vector(15 downto 0);  -- Data line to/from mem
-    DATA_OUT: out       std_logic_vector(15 downto 0);  -- Output to MEM/WB
-    clk, rst: in        std_logic                       -- Clock four times as fast as datapath to finish before memory strobes
+    AR, IN_PORT, RAM_READA : in std_logic_vector(15 downto 0);
+    WB_DATA, OUT_PORT, RAM_WRITE : out std_logic_vector(15 downto 0);
+    data_mem_sel, io_sel : in std_logic
 );
 end MemoryAccessUnit;
 
 
 architecture Behaviour of MemoryAccessUnit is
 
-component theregister is
-port(
-    clk : in std_logic;
-    d_in : in std_logic_vector(15 downto 0);
-    d_out : out std_logic_vector(15 downto 0));
-end component;
-
+signal M_DATA : std_logic_vector(15 downto 0);
 
 begin
 
-process(clk, rst)
-begin
-    if rst = '1' or opcode = x"UUUU" then
-        M_ADDR <= (others=>'0');
-        M_DATA <= (others=>'Z');
-        DATA_OUT <= (others=>'0');
-    elsif RISING_EDGE(clk) then
-        case opcode is
-            when "0100000" => -- IN
-                M_DATA <= (others=>'Z');
-                M_ADDR <= x"FFF0";
-                DATA_OUT <= M_DATA;
-
-            when "0100001" => -- OUT
-                M_ADDR <= x"FFF2";
-                M_DATA <= AR;
-
-            -- ======== NEED TO IMPLEMENT LD/STR LATER ======== --
-            --when "" =>
-
-            --when "" =>
-
+    with data_mem_sel select WB_DATA <= 
+        M_DATA when '1',
+        AR when '0';
+        
+    with IO_SEL select M_DATA <=
+        IN_PORT when '1',
+        RAM_READA when '0';
+        
+    process(io_sel) begin
+        case io_sel is
+            when '1' =>
+                OUT_PORT <= AR;
+                RAM_WRITE <= (others => 'Z');
             when others =>
-                null;
+                OUT_PORT <= (others => 'Z');
+                RAM_WRITE <= AR;
         end case;
-    end if;
+    end process;
 
 end Behaviour;
