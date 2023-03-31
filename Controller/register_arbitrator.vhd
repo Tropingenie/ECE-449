535b5bece2bfd732267c22b21a57c9d50efc6935
Kai Herrero <kaiherrero@uvic.ca>
2023-03-31 13:48:18 -0700

    Fix regression in RARB, revalidate format A with both TBs

--- Controller/register_arbitrator.vhd
+++ Controller/register_arbitrator.vhd
@@ -27,13 +27,18 @@ architecture Behavioral of RegisterArbitrator is
     type reg_array is array (integer range 0 to 7) of std_logic;
     signal reg_checkout : reg_array;
     signal read_1, read_2, ID_write, WB_write, check_in : integer;
-    signal last_instr, current_instr : std_logic_vector(15 downto 0);
+    signal last_instr, middle_instr, current_instr : std_logic_vector(15 downto 0);
+    signal single_reg_operation : std_logic := '0';
 
 begin
     read_1   <= to_integer(unsigned(ID_rd_1));
     read_2   <= to_integer(unsigned(ID_rd_2));
     ID_write <= to_integer(unsigned(id_wr));
     WB_write <= to_integer(unsigned(wb_wr));
+    
+    single_reg_operation <=
+        '1' when (read_1 = ID_write or read_2 = ID_write )and not(ID_opcode = "0000000") else
+        '0';
     
 
     process(clk, rst)
@@ -44,13 +49,18 @@ begin
             end loop;
            stall_IDEX <= '0'; 
         end if;
-            last_instr <= current_instr;
+        
+            last_instr <= middle_instr;
+            middle_instr <= current_instr; -- accounts for down clock cycle
             current_instr <= ID_opcode & ID_rd_1 & ID_rd_2 & ID_wr;
            
             -- Stall pipeline when trying to read from a checked out register, and check out the writeback register when proceeding
             case(ID_opcode) is
                 when "0000001" | "0000010" | "0000011" | "0000100" | "0000101" | "0000110" | "0100000" | "0100001"=> -- Format A that use registers
-                    if (reg_checkout(read_1) = '1' or reg_checkout(read_2) = '1') and not (current_instr = last_instr) then
+                    if (
+                        (reg_checkout(read_1) = '1' or reg_checkout(read_2) = '1') 
+                        and (single_reg_operation = '0' or not (current_instr = last_instr))
+                        ) then
                         stall_IDEX <= '1';
                     else
                         if not(ID_opcode = "0100000") then
